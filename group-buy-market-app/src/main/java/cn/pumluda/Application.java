035cc8d738e64720810e6298f2ddf84b9984f43e
PenguinJP <1637435385@qq.com>
2026-06-02 12:14:02 +0800

    feat: 增加 Spring kafka 消息队列
    todo: 添加超时关单、关团、优惠名额恢复兜底补偿机制

--- group-buy-market-app/src/main/java/cn/pumluda/Application.java
+++ group-buy-market-app/src/main/java/cn/pumluda/Application.java
@@ -3,12 +3,14 @@ package cn.pumluda;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @Configurable
 public class Application {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
 
