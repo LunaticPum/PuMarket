1b149fafeb8f26628ea70f7907595e06dca9cb71
PenguinJP <1637435385@qq.com>
2026-06-10 19:18:35 +0800

    feat:  完善项目，合并Agent功能分支

---
+++ group-buy-market-api/src/main/java/cn/pumluda/api/dto/GetIndexInfoReqDTO.java
@@ -0,0 +1,42 @@
+package cn.pumluda.api.dto;
+
+import lombok.Data;
+
+
+/**
+ * Project: group-buy-market-better <p>
+ * File: CreateOrderReqDTO <p>
+ * Created by: 16374 <p>
+ * Date: 2026/5/26 <p>
+ * Time: 17:28 <p>
+ * Description: 创建订单请求 DTO
+ */
+@Data
+public class CreateOrderReqDTO {
+
+    /* 用户 ID */
+    private Long userId;
+
+    /* 商品 ID */
+    private Long skuId;
+
+    /* 商品购买数量 */
+    private int quantity;
+
+    /* 交易单号：由外部系统生成并传入 */
+    private String orderNo;
+
+    /* 活动 ID */
+    private Long activityId;
+
+    /* 拼团队伍 ID */
+    private Long groupTeamId;
+
+    /* 流量来源 */
+    private Integer entrySource;
+
+    /* 结算订单请求才有：交易渠道 */
+//    private String channel;
+
+}
+
